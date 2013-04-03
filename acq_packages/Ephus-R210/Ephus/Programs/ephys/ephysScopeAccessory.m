c85707a720fe65facee3d938323c8adeb31da67f
andrew giessel <andrew.giessel@gmail.com>
2013-04-03 17:34:24 -0500

    changed to 5% on either side for expand button

--- acq_packages/Ephus-R210/Ephus/Programs/ephys/ephysScopeAccessory.m
+++ acq_packages/Ephus-R210/Ephus/Programs/ephys/ephysScopeAccessory.m
@@ -871,7 +871,7 @@ sc = getGlobal(progmanager, 'scopeObject', 'scopeGui', 'scopeGui');
 ylim = get(get(get(sc, 'figure'), 'CurrentAxes'), 'YLim');
 
 yrange = ylim(2) - ylim(1);
-amount_to_expand = floor(yrange * 0.1);
+amount_to_expand = floor(yrange * 0.05);
 
 if getLocal(progmanager, hObject, 'freezeButton')
     if getLocal(progmanager, hObject, 'startButton')
