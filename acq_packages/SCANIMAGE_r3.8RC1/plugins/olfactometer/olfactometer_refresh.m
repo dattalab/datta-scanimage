4da37a9c288eab16f2ffdf71daa9874332dccee3
andrew giessel <andrew@giessel.com>
2014-11-04 13:51:51 -0500

    Update olfactometer_refresh.m
    took out commented code.
    
    runs during init, and when the refresh button is hit.

--- acq_packages/SCANIMAGE_r3.8RC1/plugins/olfactometer/olfactometer_refresh.m
+++ acq_packages/SCANIMAGE_r3.8RC1/plugins/olfactometer/olfactometer_refresh.m
@@ -2,6 +2,4 @@ function olfactometer_refresh(vargin)
 global state;
 calculateFrameTimes();
 buildOdorStateTransitions();
-%overrideFrames(state.olfactometer.nFrames);
-%acq_setTraceLength(state.acq.numberOfFrames / state.acq.frameRate);
 end
